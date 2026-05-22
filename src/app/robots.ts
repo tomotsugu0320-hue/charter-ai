d37d9ebce310df477a027b36e1fefaee9d99d491
tomotsugu0320-hue <tomotsugu0210@gmail.com>
2026-05-22 15:02:54 +0900

    noindex forum admin pages

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -16,6 +16,7 @@ export default function robots(): MetadataRoute.Robots {
           `/${defaultTenant}/forum/thread/*`,
         ],
         disallow: [
+          `/${defaultTenant}/forum/admin`,
           `/${defaultTenant}/forum/admin/*`,
           "/api/*",
           "/_next/*",
